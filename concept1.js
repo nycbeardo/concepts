0f22a18fd6cd65b1306d5beec3c48e2afb17a5d6
Montique Stevens <mstevens528@gmail.com>
2023-05-23 13:26:26 -0400

    Update concept1.js
    added background color to canvas

--- concept1.js
+++ concept1.js
@@ -1,4 +1,9 @@
 function setup(){
-    createCanvas(800, 800);
+    createCanvas(800, 800); // size of canvas for drawing
 
+}
+
+function draw () {
+    background (200); // background color for whole canvas 
+    
 }
